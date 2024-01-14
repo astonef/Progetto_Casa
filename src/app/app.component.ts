b3f0696316d768281606c088934599468d4c3883
Stefano <stefano.g.magni@hotmail.it>
2024-01-14 16:42:29 +0100

    Aggiunti file di progetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HomeComponent
+  ],
 })
 export class AppComponent {
-  title = 'tutorial';
+  title = 'Progetto Casa';
 }
